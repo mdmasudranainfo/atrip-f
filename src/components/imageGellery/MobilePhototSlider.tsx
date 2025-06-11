656e13b877a0f18a31dfc38ab34bb44a23ca544b
masudranainfo <mdmasude7@gmail.com>
2025-06-11 16:10:25 +0600

    play video

--- src/components/imageGellery/MobilePhototSlider.tsx
+++ src/components/imageGellery/MobilePhototSlider.tsx
@@ -1,4 +1,3 @@
-// components/EmblaCarousel.tsx
 "use client";
 
 import React, {
@@ -11,6 +10,7 @@ import React, {
 import useEmblaCarousel from "embla-carousel-react";
 import Image from "next/image";
 import { EventRow } from "@/types/activity";
+import ReactPlayer from "react-player";
 
 interface Props {
   data: EventRow;
@@ -25,6 +25,9 @@ const MobilePhototSlider: React.FC<Props> = ({
   showGallery,
   setShowGallery,
 }: any) => {
+  const hasVideo = Boolean(data?.video);
+  const slides2 = hasVideo ? [{ type: "video" }, ...slides] : slides;
+
   const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
   const [selectedIndex, setSelectedIndex] = useState(0);
 
@@ -34,18 +37,6 @@ const MobilePhototSlider: React.FC<Props> = ({
     },
     [emblaApi]
   );
-
-  // Auto slide effect
-  useEffect(() => {
-    if (!emblaApi) return;
-    const interval = setInterval(() => {
-      if (!emblaApi) return;
-      const nextIndex = (emblaApi.selectedScrollSnap() + 1) % slides.length;
-      emblaApi.scrollTo(nextIndex);
-    }, 3000); // Change slide every 3 seconds
-
-    return () => clearInterval(interval);
-  }, [emblaApi, slides.length]);
 
   useEffect(() => {
     if (!emblaApi) return;
@@ -62,27 +53,42 @@ const MobilePhototSlider: React.FC<Props> = ({
     <div className="relative rounded-xl overflow-hidden shadow-lg">
       <div className="overflow-hidden" ref={emblaRef}>
         <div className="flex">
-          {slides.map((slider: any, index: number) => (
+          {slides2.map((slider: any, index: number) => (
             <div
-              className="min-w-full h-[400px] aspect-video relative"
+              className="min-w-full h-[400px] aspect-video relative flex-shrink-0"
               key={index}
             >
-              <Image
-                src={slider.large}
-                alt={`Slide ${index}`}
-                fill
-                className="object-cover cursor-pointer"
-                onClick={() => setShowGallery(true)}
-              />
+              {slider?.type === "video" ? (
+                <div className="h-full pointer-events-auto">
+                  <ReactPlayer
+                    url={`${data?.video}?rel=0`}
+                    playing={selectedIndex === index}
+                    controls
+                    width="100%"
+                    height="100%"
+                    muted
+                    className="object-cover"
+                  />
+                </div>
+              ) : (
+                <Image
+                  src={slider.large}
+                  alt={`Slide ${index}`}
+                  fill
+                  className="object-cover cursor-pointer"
+                  onClick={() => setShowGallery(true)}
+                />
+              )}
             </div>
           ))}
         </div>
       </div>
+
       {/* Gradient overlay at bottom */}
-      <div className="absolute inset-x-0 bottom-0 photoGellery text-white p-4 pt-8">
+      <div className="absolute inset-x-0 bottom-0 photoGellery text-white p-4 pt-8 z-40">
         {/* Dot navigation */}
-        <div className=" flex gap-2 mb-1">
-          {slides.map((_: any, index: number) => (
+        <div className="flex gap-2 mb-1">
+          {slides2.map((_: any, index: number) => (
             <button
               key={index}
               onClick={() => scrollTo(index)}
@@ -93,7 +99,7 @@ const MobilePhototSlider: React.FC<Props> = ({
           ))}
         </div>
 
-        <h3 className="text-lg font-bold leading-tight ">{data?.title}</h3>
+        <h3 className="text-lg font-bold leading-tight">{data?.title}</h3>
         <p className="text-sm">
           <span>{data.address}</span>
         </p>
